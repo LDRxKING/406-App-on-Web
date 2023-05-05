939f5cd4d8b0a24d65de84655b6d1d4aafb3c382
LDRxKING <125299559+LDRxKING@users.noreply.github.com>
2023-05-05 16:58:07 -0500

    Update Project_7.py

--- Project_7.py
+++ Project_7.py
@@ -6,8 +6,9 @@ import dash_bootstrap_components as dbc
 import csv
 from pathlib import Path
 
+
+
 file_path = Path('C:/Users/switz/OneDrive/Desktop/Python Code/USA_cars_datasets.csv')
-file_path = directory_path / 'USA_cars_datasets.csv'
 
 usa_cars_data= pd.read_csv(file_path)
 
